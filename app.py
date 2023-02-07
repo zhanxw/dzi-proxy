ba0046ae9e734eec7b987ed25f8efd8ce3e8c9ae
zhanxw <zhanxw@gmail.com>
2023-02-07 15:39:05 -0600

    init checkin

---
+++ app.py
@@ -0,0 +1,159 @@
+from flask import Flask, abort, make_response, render_template, url_for
+from flask_cors import CORS, cross_origin
+import io
+from optparse import OptionParser
+import requests
+
+pathPrefix = '/Users/zhanxw/Downloads/test.tiff/'
+urlPrefix = 'http://localhost:8000/'
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/<path:path>.dzi')
+@cross_origin()
+def dzi(path):
+    
+    resp = make_response(open(pathPrefix + '10021.dzi.dzi').read())
+    resp.mimetype = 'application/text'
+    return resp
+
+
+@app.route('/<path:path>_files/<int:level>/<int:col>_<int:row>.<format>')
+@cross_origin()
+def tile(path, level, col, row, format):
+    print("request: ", path, level, col, row, format)
+    fn = pathPrefix + '10021.dzi_files/%d/%d_%d.%s' % (level, col, row, format)
+    resp = make_response(open(fn, 'rb').read())
+    resp.mimetype = 'image/jpeg'
+    return resp
+
+@app.route('/proxy/<path:path>.dzi')
+@cross_origin()
+def proxy_dzi(path):
+    print("proxy: " + path)
+    resp = make_response(requests.get(urlPrefix + path + ".dzi").text)
+    resp.mimetype = 'application/text'
+    return resp
+
+pathPrefix = '/Users/zhanxw/Downloads/test.tiff/'
+tiffFile = pathPrefix + '10021.svs'
+
+import numpy
+from tifffile import TiffFile
+import math
+def get_svs_tile(level, col, row):
+    ## find the tiff image to work on
+
+    ## find tiff tiles 
+    ## extract images
+    ## return results
+
+    # return None
+
+    t = TiffFile(tiffFile)
+    p0 = t.pages[0]
+    p0_size = [p0.imagewidth, p0.imagelength]
+    p3 = t.pages[3]
+    p3_size = [p3.imagewidth, p3.imagelength]
+    page  = [p for p in t.pages]
+    page_size = [ (p.imagewidth, p.imagelength) for p in page ]
+    s = (p0.imagewidth, p0.imagelength)
+    dims = []
+    while True:
+        dims.append(s)
+        i = (math.ceil(s[0]/2), math.ceil(s[1]/2))
+        if i[0] == 1 and i[1] == 1: 
+            break
+        else:
+            s = i
+    dims.append(i)
+    # level = 5
+    # col = 1
+    # row = 2
+    ref_page_size = p3
+    requested_dims = dims[-(level+1)]
+
+    ref = [ p for p in t.pages if p.imagewidth >= requested_dims[0]][-1]
+    ref_size = [ref.imagewidth, ref.imagelength]
+    scale = ref_size[0] / requested_dims[0]
+    print("scale = ", scale, ref_size[0], requested_dims[0])
+    request_x = col 
+    request_y = row
+    page_x = min(request_x * 255 * scale, ref_size[0])
+    page_y = min(request_y * 255 * scale, ref_size[1])
+    page_width = min(255 * scale, ref_size[0] - page_x - 1)
+    page_height = min(255 * scale, ref_size[1] - page_y - 1)
+    (page_x, page_y, page_width, page_height) = map(int, (page_x, page_y, page_width, page_height))
+
+    import read_region
+    # ret = read_region.read_region(p0, 100, 200, 255, 255)
+    print("read_region:", ref_size, page_y, page_x, page_height, page_width )
+    ret = read_region.read_region(ref, page_y, page_x, page_height, page_width)
+    # success, frame = vidcap.read()
+    import cv2
+    ret_s = cv2.resize(ret[0,:], (255, 255))
+    _, JPEG = cv2.imencode('.jpeg', ret_s)
+    return JPEG.tobytes()
+
+@app.route('/proxy/<path:path>_files/<int:level>/<int:col>_<int:row>.<format>')
+@cross_origin()
+def proxy_tile(path, level, col, row, format):
+    print("proxy request: ", path, level, col, row, format)
+
+    fn = pathPrefix + '10021.dzi_files/%d/%d_%d.%s' % (level, col, row, format)
+
+    resp = make_response(get_svs_tile(level, col, row))
+    resp.mimetype = 'image/jpeg'
+    return resp
+
+
+    slide, masks = _get_slide(path)
+    format = format.lower()
+    if format != 'jpeg' and format != 'png':
+        # Not supported by Deep Zoom
+        abort(404)
+    try:
+        tile = slide.get_tile(level, (col, row))
+        if masks is not None:
+            mask = masks.get_tile(level, (col, row))
+        else:
+            if level == slide._dz_levels-1 and app.model is not None:
+                mask = run_tile(tile, app.model, app.dataset_configs, mpp=None)
+                mask = Image.fromarray(mask)
+            else:
+                mask = None
+    except ValueError:
+        # Invalid level or coordinates
+        abort(404)
+
+    buf = BytesIO()
+    if mask is not None:
+        tile.paste(mask, mask=mask.split()[-1])
+    tile.save(buf, format, quality=app.config['DEEPZOOM_TILE_QUALITY'])
+    resp = make_response(buf.getvalue())
+    resp.mimetype = 'image/%s' % format
+    return resp
+
+
+if __name__ == '__main__':
+    parser = OptionParser(usage='Usage: %prog [options] [slide-directory]')
+    parser.add_option(
+        '-l',
+        '--listen',
+        metavar='ADDRESS',
+        dest='host',
+        default='0.0.0.0',
+        help='address to listen on [127.0.0.1]',
+    )
+    parser.add_option(
+        '-p',
+        '--port',
+        metavar='PORT',
+        dest='port',
+        type='int',
+        default=5000,
+        help='port to listen on [5000]',
+    )
+    (opts, args) = parser.parse_args()
+    app.run(host=opts.host, port=opts.port, threaded=True)    
---
+++ check.tiff.py
@@ -0,0 +1,228 @@
+# jello.tif	256x192 8-bit RGB (lzw palette) Paul Heckbert "jello"
+fn = "libtiffpic/jello.tif"
+import sys
+if len(sys.argv) > 1:
+    fn = sys.argv[1]
+
+f = open(fn, 'rb')
+from struct import *
+header = f.read(2)
+print(header)
+if header == b'MM':
+    print("Big endian")
+    endian = ">"
+elif header == b'II':
+    print("Little endian")
+    endian = "<"
+else:
+    print("something wrong")
+
+magic = f.read(2)
+print(magic)
+import struct
+[ord(i) for i in struct.unpack_from(endian+"cc",magic)]  ## should be [0, 42]
+
+offset = f.read(4)
+offset = struct.unpack_from(endian + "i", offset)[0]
+
+TYPE_DICT = {1:'BYTE', 2:'ASCII', 3:"SHORT", 4: "LONG", 5: 'RATIONAL',  6:'SBYTE', 7:'UNDEFINED', 8:'SSHORT', 9:'SLONG', 10: 'SRATIONAL', 11:'FLOAT', 12:'DOUBLE'}
+TAG_DICT = {
+    254:'NewSubfileType',
+    255:'SubfileType',
+    256:'ImageWidth',
+    257:'ImageLength', 
+    258:'BitsPerSample', 
+    259:'Compression', #1: no compression; 2: CCITT Group 3 1-D Huffman RLE encoding; 32773: PackBits compression
+    262:'PhotometricInterpretation', #3: Palette color
+    263:'Threshholding',
+    270:'ImageDescription',
+    273:'StripOffsets',
+    274:'Orientation',
+    277:'SamplesPerPixel',
+    278:'RowsPerStrip',
+    279:'StripByteCounts',
+    284:'PlanarConfiguration',
+    320:'ColorMap',
+    322:'TileWidth',
+    323:'TileLength',
+    324:'TileOffsets',
+    325:'TileByteCounts',
+    347:'JPEGTables',
+    530:'YCbCrSubsampling',
+    32997:'ImageDepth',
+    34675:'ICC Profile',
+}
+
+while offset != 0:
+    print("-"*25 + str(offset) + " " + "0x%06x" % offset + "-"*25)
+    ret = f.seek(offset, 0)
+    num_d_entries = f.read(2)
+    num_d_entries = struct.unpack_from(endian + "H", num_d_entries)[0]
+    print(num_d_entries, "entries")
+
+    entries = [f.read(12) for i in range(num_d_entries)]
+    next_offset = f.read(4)
+    next_offset = struct.unpack_from(endian + "I", next_offset)[0]
+
+    entries = [ (e[:2], e[2:4], e[4:8], e[8:12]) for e in entries] # tag (sorted), type, count, value/offset
+    # struct.unpack_from(endian + "h", entries[0][1])[0] 
+
+    entries = [ (struct.unpack_from(endian + "H", e[0])[0], # tag
+                TYPE_DICT[struct.unpack_from(endian + "H", e[1])[0]], # type
+                struct.unpack_from(endian + "I", e[2])[0], # count
+                e[3]) for e in entries # value/offset
+                ]  
+    entriesDict = {}
+    for idx, val in enumerate(entries):
+        tag, type, count, value = val
+        if type == 'SHORT':
+            v = struct.unpack_from(endian + "H", entries[idx][3])[0]
+            o = struct.unpack_from(endian + "I", entries[idx][3])[0]
+        elif type == 'LONG': ## TODO: need to check tag to determine offset or value
+            v = struct.unpack_from(endian + "I", entries[idx][3])[0]
+            o = v
+        elif type == 'ASCII':
+            o = struct.unpack_from(endian + "I", entries[idx][3])[0]
+            f.seek(o, 0)
+            v = f.read(entries[idx][2])
+            v = struct.unpack_from(endian + str(entries[idx][2]) + "c", v)
+            v = v[:-1] # get ride of \0
+            v = ''.join([i.decode() for i in v])
+        else:
+            v = entries[idx][3]
+            o = struct.unpack_from(endian + "I", entries[idx][3])[0]
+        entries[idx] = (str(tag) + ":" + TAG_DICT.get(tag, "***") , type, count, v, o)
+        entriesDict[entries[idx][0]] = (type, count, v, o)
+
+    
+    for idx, val in enumerate(entries):
+        tag, type, count, v, o = val
+        val = tag, type, str(count), str(v), str(o)
+        # print(val)
+        print('%32s\t%s\t%s\t%s\t%s' % val)
+
+    print("next_offset = %d 0x%06x" % (next_offset, next_offset))
+
+    offset = next_offset
+
+# f.close()
+
+def getStripInfo(entries):
+    offset = []
+    bytes = []
+    for e in entries:
+        if e[0] == '273:StripOffsets':
+            f.seek(e[3])
+            for i in range(e[2]):
+                assert(e[1] == 'LONG')
+                v = f.read(4)
+                offset.append(struct.unpack_from(endian + "I", v)[0])
+        if e[0] == '279:StripByteCounts':
+            f.seek(e[3])
+            for i in range(e[2]):
+                assert(e[1] == 'LONG')
+                v = f.read(4)
+                bytes.append(struct.unpack_from(endian + "I", v)[0])    
+    return (offset, bytes)
+def getStrips(offsets, bytes):
+    res = []
+    for o, b in zip(offsets, bytes):
+        f.seek(o)
+        res.append(f.read(b))
+    return res
+
+# from:
+# https://github.com/psd-tools/packbits/blob/master/LICENSE.txt
+# https://github.com/psd-tools/packbits/blob/master/src/packbits.py
+def decode(data):
+    """
+    Decodes a PackBit encoded data.
+    """
+    data = bytearray(data) # <- python 2/3 compatibility fix
+    result = bytearray()
+    pos = 0
+    while pos < len(data):
+        header_byte = data[pos]
+        if header_byte > 127:
+            header_byte -= 256
+        pos += 1
+        if 0 <= header_byte <= 127:
+            result.extend(data[pos:pos+header_byte+1])
+            pos += header_byte+1
+        elif header_byte == -128:
+            pass
+        else:
+            result.extend([data[pos]] * (1 - header_byte))
+            pos += 1
+    #return bytes(result)
+    return result
+
+## 262:PhotometricInterpretation
+##   0: WhiteIsZero
+##   1: BlackIsZero
+##   2: RGB
+##   3: Palette color
+##   4: Transparency Mask
+## for palette color
+if entriesDict.get('262:PhotometricInterpretation')[2] == 3: ## Palette color
+    (offsets, bytes) = getStripInfo(entries)
+    res = getStrips(offsets, bytes)
+    res2 = [decode(d) for d in res]
+    [len(r) for r in res2]
+    [max(r) for r in res2]
+    [min(r) for r in res2]
+    # read colormap
+    # f.seek(45600)
+    type, count, value, offset = entriesDict.get('320:ColorMap')
+    print(entriesDict.get('320:ColorMap'))
+    f.seek(offset)
+    cmap = f.read(count * 2) ## read by 16-bit
+    cmap = struct.unpack_from(endian + str(count) + "H", cmap)
+    cmap = [(cmap[i], cmap[count//3 + i ], cmap[count//3*2 + i]) for i in range(count//3)]
+
+    ## 
+    idx = []
+    for r in res2:
+        for rr in r:
+            idx.append(rr)
+    import numpy as np
+    from PIL import Image as im
+    img = np.array([cmap[i] for i in idx])
+    img = img.reshape((entriesDict['257:ImageLength'][2], entriesDict['256:ImageWidth'][2],3))
+    img = img / 65535 * 255
+    print(img.shape) # 8192 x 3
+    imgH = im.fromarray(np.uint8(img), mode = 'RGB')
+    imgH.show()
+
+if entriesDict.get('262:PhotometricInterpretation')[2] == 2: ## RGB
+    if entriesDict.get('259:Compression')[2] == 7: ## JPEG compression
+        print("JPEG")
+        (offsets, bytes) = getStripInfo(entries)
+        res = getStrips(offsets, bytes)
+       
+        data = res[0]
+        from struct import unpack
+        marker_mapping = { \
+            0xffd8: "Start of Image", \
+            0xffe0: "Application Default Header", \
+            0xffdb: "Quantization Table", \
+            0xffc0: "Start of Frame", \
+            0xffc4: "Define Huffman Table",\
+            0xffda: "Start of Scan",\
+            0xffd9: "End of Image"\
+        }
+        while(True):
+            marker, = unpack(">H", data[0:2])
+            print(marker_mapping.get(marker))
+            if marker == 0xffd8:
+                data = data[2:]
+            elif marker == 0xffd9:
+                print('return')
+                break
+            elif marker == 0xffda:
+                data = data[-2:]
+            else:
+                lenchunk, = unpack(">H", data[2:4])
+                data = data[2+lenchunk:]            
+            if len(data)==0:
+                break       
